31aefd84d82918b77626140db68cd6c890c564ad
vmramirez5 <vmramirez5@cps.edu>
2025-01-07 14:49:40 -0600

    Update 1_.py

--- 1_.py
+++ 1_.py
@@ -1,34 +1,59 @@
-# Methods, Help & Documentation Practice #1
-# Remove the characters to the left of our main text:
+from determineEligibilty import determineEligibilty_toGraduate, listOfMovies
+# # functions are ways to wrap your code into reusable units
 
-# ,
+# function = A block of reusable code 
+#            place () after the function name to invoke it
 
-# :
+# #define a function with def
+# # only define the function once
+# # when I pass ini a parameter,
+# # I am passing in a place holder
+# # for future information
+# def say_hello(name,age,address):
+#     print (f"Hello {name}!")
+#     print (f"How are you {name}?")
+#     print(f"{name} is {age} years old")
+#     print(f"{name} lives in {address}")
 
-# %
-
-# _
-
-# #
-
-# Use the lstrip() method. Print the result to the screen:
-
-# ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
-
-# Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+# #call the function
+# #pass in the information called an argument
+# say_hello("Dom", 12, '123 Main St')
+# say_hello("Paul", 192,'456 Main St')
+# say_hello("Bob", 3000, '789 Main St')
 
 
-# Methods, Help & Documentation Practice #2
-# Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
 
-# fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+determineEligibilty_toGraduate("Alice", 120, 2.0, 800)
+determineEligibilty_toGraduate("Bob", 119, 1.9, 799)
 
-# Search the documentation for the requested method to know how it works.
+listOfMovies("The Matrix", 10, "action")
+listOfMovies("The Hangover", 5, "comedy")
+listOfMovies("The Ring", 3, "horror")
+listOfMovies("Mufasa", 2, "animated")
 
-# Methods, Help & Documentation Practice #3
-# Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
+# the return statement is used to return a value from a function
 
-# phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-# tv_brands = {"Sony", "Philips", "Samsung", "LG"}
-# Search the documentation for the requested method to know how it works.
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# return = statement used ti end a function  and send a result back to caller
+
+def add(x,y):
+    z = x + y
+    return z
+
+def subtract(x,y):
+    z = x - y
+    return z
+
+def multiply(x,y):
+    z = x * y
+    return z
+
+def divide(x,y):
+    z = x / y
+    return z
+
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
